e8567dc497e0af96025515a9e6efdc63f44ed889
RehabMan <racerrehabman@gmail.com>
2014-02-15 07:26:42 -0800

    fix issue #1: clean up debug message

--- ACPIDebug/ACPIDebug.cpp
+++ ACPIDebug/ACPIDebug.cpp
@@ -114,7 +114,7 @@ IOReturn ACPIDebug::message(UInt32 type, IOService * provider, void * argument)
 {
 	if (type == kIOACPIMessageDeviceNotification)
 	{
-        IOLog("ACPIDebug::message(%d, %p, %p)\n", type, provider, argument);
+        DEBUG_LOG("ACPIDebug::message(%d, %p, %p)\n", type, provider, argument);
         PrintTraces();
 	}
 	return super::message(type, provider, argument);
